dd37dcbf92d03dac3df4e5f215dbd41229cb9d0c
Harshneet Bhatia <hb9ee@virginia.edu>
2020-04-15 00:02:51 -0400

    delete unnecessary file (mapScript.js)

--- app/templates/app/mapScript.js
+++
@@ -1,9 +0,0 @@
-var map;
-function createMap(){
-    var options = {
-        center: {lat: 43.654, lng: -79.383},
-        zoom = 10
-    };
-
-    map = new google.map.Maps(document.getElementById('map'), options);
-}
